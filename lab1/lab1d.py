f79710c4bca46258daaf1d70b5b768cb1066fd8b
Tandin <twangmo12@myseneca.ca>
2024-09-19 00:41:41 -0400

    Added lab1d

---
+++ lab1/lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define the objects with integer values
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and print the result
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
