64a1a0c7e3673cfcac3214fe66bbd3352e05130e
Tandin <twangmo12@myseneca.ca>
2024-09-25 12:10:43 -0400

    Added Scripts to lab2c

---
+++ lab2/lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Assign command line arguments to variables
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+# Print the output
+print(f"Hello {name}, you are {age} years old.")
