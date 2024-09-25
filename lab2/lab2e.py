0d8017e3fb2130e44cc19a34eef3f1cf54f17934
Tandin <twangmo12@myseneca.ca>
2024-09-25 13:33:22 -0400

    Added Scripts to lab2e

--- lab2/lab2e.py
+++ lab2/lab2e.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-
-# Author: Tandin
+# Author: Tandin Wangmo
 # Author ID: twangmo12
 # Date Created: 2024/09/25
 
-timer = 10
+import time
 
-while timer > 0:
-    print(f"{timer} seconds remaining...")
-    timer -= 1
+# Set the countdown start value
+countdown_start = 10
 
-print("Time's up!")
+# Countdown loop
+while countdown_start > 0:
+    print(countdown_start)  # Print the current countdown number
+    time.sleep(1)           # Wait for 1 second
+    countdown_start -= 1     # Decrease the countdown number
+
+print("blast off!")  # Print "blast off!" after the countdown ends
