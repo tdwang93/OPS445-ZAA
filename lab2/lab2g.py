a43d04a078a0701aade21b666dbab1f4f485e0b2
Tandin <twangmo12@myseneca.ca>
2024-09-25 13:01:17 -0400

    completed lab2

--- lab2/lab2g.py
+++ lab2/lab2g.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-
-# Author: Tandin
+# Author: Tandin Wangmo
 # Author ID: twangmo12
 # Date Created: 2024/09/25
 
 import sys
 
-# Check if an argument was provided
+# Check if an argument was entered; if not, set the timer to 3
 if len(sys.argv) > 1:
-    timer = int(sys.argv[1])  # Assign the first command line argument to timer
+    timer = int(sys.argv[1])  # Use the provided argument for the timer
 else:
     timer = 3  # Default timer value
 
+# While loop that repeats until timer equals 0
 while timer > 0:
-    print(f"{timer} seconds remaining...")
-    timer -= 1
+    print(timer)  # Print the current timer value
+    timer -= 1    # Decrement the timer
 
-print("Time's up!")
+print("blast off!")  # Final message after the countdown
