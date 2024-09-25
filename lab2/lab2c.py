1e2ba7a314f0849abe2ca314081b4ab4f412f6e0
Tandin <twangmo12@myseneca.ca>
2024-09-25 13:07:00 -0400

    Added Scripts to lab2c

--- lab2/lab2c.py
+++ lab2/lab2c.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+# Author: Your Full Name
+# Author ID: your_seneca_id
+# Date Created: yyyy/mm/dd
 
 import sys
 
-# Assign command line arguments to variables
-name = sys.argv[1]
-age = int(sys.argv[2])
+# Assign the command line arguments to variables
+name = sys.argv[1]  # First argument as the name
+age = int(sys.argv[2])  # Second argument as the age, converted to integer
 
-# Print the output
-print(f"Hello {name}, you are {age} years old.")
+# Print the required output
+print(f"Hi {name}, you are {age} years old.")
