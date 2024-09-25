f817a67ce9e59e5adae3a444148d5fec3df1932a
Tandin <twangmo12@myseneca.ca>
2024-09-25 12:16:48 -0400

    Added Scripts to lab2e

---
+++ lab2/lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Tandin
+# Author ID: [your_seneca_id]
+# Date Created: 2024/09/25
+
+timer = 10
+
+while timer > 0:
+    print(f"{timer} seconds remaining...")
+    timer -= 1
+
+print("Time's up!")
