318b1a509796784f612b4f90899c8b8dfa8aed74
Tandin <twangmo12@myseneca.ca>
2024-09-25 13:40:01 -0400

    Completed Lab2

--- lab2/lab2f.py
+++ lab2/lab2f.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-
-# Author: Tandin
+# Author: Tandin Wangmo
 # Author ID: twangmo12
-# Date Created: 2024/09/25
+# Date Created: 2024/09/24
 
 import sys
+import time
 
-# Assign the command line argument to the timer variable
-timer = int(sys.argv[1])
+# Check if an argument is provided
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <number>")
+    sys.exit(1)
 
-while timer > 0:
-    print(f"{timer} seconds remaining...")
-    timer -= 1
+try:
+    countdown_start = int(sys.argv[1])  # Convert the argument to an integer
+except ValueError:
+    print("Please provide a valid integer.")
+    sys.exit(1)
 
-print("Time's up!")
+# Countdown loop
+while countdown_start > 0:
+    print(countdown_start)  # Print the current countdown number
+    time.sleep(1)           # Wait for 1 second
+    countdown_start -= 1     # Decrease the countdown number
+
+print("blast off!")  # Print "blast off!" after the countdown ends
